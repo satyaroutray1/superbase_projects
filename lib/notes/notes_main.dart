84bba2eebc345a565e185f841ea2c4ff7b26edbb
satya <sroutraykec@gmail.com>
2025-07-18 11:34:09 +0530

    Initial commit

---
+++ lib/notes/notes_main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: Center(
+        child: Text('data'),
+      )
+    ));
+  }
+}
+
