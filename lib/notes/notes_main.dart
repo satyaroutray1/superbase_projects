b3a9c9e80bf147be1dede1a1134434ff531b54a1
satya <sroutraykec@gmail.com>
2025-07-19 22:24:30 +0530

    wip

--- lib/notes/notes_main.dart
+++ lib/notes/notes_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:superbase_projects/notes/screens/home_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,18 +24,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-        appBar: AppBar(title: Text('Notes App'),),
-        body: Center(
-        child: Text('data'),
-      )
-    ));
-  }
-}
-
